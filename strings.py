52e5f39a58f4afd39f76a2a385e9f688c17359f6
shreeya tamang <121777681+shreeyatamang@users.noreply.github.com>
2023-05-29 21:17:47 +0545

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+name="shreeya"
+print("hello,%s!" % name)
+
+name="shreeya tamang"
+age=19
+print("%s is %d years old." %(name,age))
+
+data=("alisha","khadka",60)
+format_string="hello %s %s. your current balance is $%s"
+print(format_string %data)
+
+data=("preetika","gurung",70)
+format_string="hi %s %s. your current balance is $%s"
+print(format_string %data)
+
