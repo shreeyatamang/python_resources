d3c999dc596628276edf48da20c9ef420f953d8a
shreeya tamang <121777681+shreeyatamang@users.noreply.github.com>
2023-05-29 20:40:29 +0545

    Add files via upload

---
+++ pie.py
@@ -0,0 +1,19 @@
+print("Hello This is shreeya Tamang")
+print("let's start with variables")
+myint=7
+print(myint)
+myfloat=14.4
+print(myfloat)
+mylist = [2,3,4,5]
+print(mylist[2])
+print("Now move to arthimetic ")
+numbers = 1+2%3*6
+print(numbers)
+square=7**8
+cube=7**8
+print(square)
+print(cube)
+remainder=5%6
+print(remainder)
+
+
