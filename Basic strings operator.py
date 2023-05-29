95f4e2b50ed59f3eecb037b43f8c4559044c0270
shreeya tamang <121777681+shreeyatamang@users.noreply.github.com>
2023-05-29 21:45:15 +0545

    Add files via upload

---
+++ Basic strings operator.py
@@ -0,0 +1,27 @@
+astring="hello world!"
+print("single quotes are ' ' ")
+print(len(astring))
+
+astring="hello world!"
+print("double quotes are  " " ")
+print(len(astring))
+
+astring="hello world!"
+print(astring.index("o"))
+
+astring="hello world!"
+print(astring.count("1"))
+
+astring="hello world!"
+print(astring[3:7])
+
+astring="hello world!"
+print(astring[3:7:2])
+
+astring="hello world!"
+print(astring[3:7])
+print(astring[3:7:1])
+
+astring="shreeyatamang!"
+print(astring.upper())
+print(astring.lower())
