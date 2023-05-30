90da4ef44df222502b199983c9ad13f04e9e8024
shreeya tamang <121777681+shreeyatamang@users.noreply.github.com>
2023-05-30 11:12:07 +0545

    Add files via upload

---
+++ Boolean condition.py
@@ -0,0 +1,7 @@
+name = "shreeya"
+age = 20
+if name == "shreeya" and age == 20 :
+    print("your name is shreeya and you are aslo 20 years old")
+    
+if name == "shreeya" or name == "alisha" :
+  print("your name is either shreeya or alisha")
