b1bab24370815e2a3f9719c5d83da2639e50386b
shreeya tamang <121777681+shreeyatamang@users.noreply.github.com>
2023-05-30 11:45:35 +0545

    Add files via upload

---
+++ In,Is,Not operator.py
@@ -0,0 +1,47 @@
+name = "shreeya"
+if name in ["shreeya", "alisha"]:
+  print("your name is either shreeya or alisha")
+  
+  x=[2,3,4]
+  y=[2,3,4]
+  print(x==y) #prints out true
+  print(x is y) #prints out false
+  
+  
+  print(not false) #prints out true
+  print((not false)==(false)) #prints out false
+  
+
+  number=16
+  second_number=0
+  first_array=[1,2,3]
+  second_array=[1,2]
+  
+  if number>15:
+      print("1")
+  
+  
+  if first_array:
+      print("2")
+      
+      if len(second_array) == 2:
+          print("3")
+          
+          if len(second_array) + len(first_array) == 5:
+              print("4")
+              if first_array and first_array[0] == 1:
+                  print("5")
+                  if not second_number :
+                      print("6")
+                  
+          
+      
+  
+  
+  
+  
+  
+  
+  
+  
+  
